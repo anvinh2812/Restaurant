d40b55e91cb1f297f4d723de10777d024360c030
anvinh2812 <andangvinh2812.work@gmail.com>
2024-12-06 21:40:16 +0700

    restaurant real-time done

---
+++ rms.sql
@@ -0,0 +1,257 @@
+
+--
+-- Database: `rms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_item_table`
+--
+
+CREATE TABLE `order_item_table` (
+  `order_item_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `product_quantity` int(4) NOT NULL,
+  `product_rate` decimal(12,2) NOT NULL,
+  `product_amount` decimal(12,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_table`
+--
+
+CREATE TABLE `order_table` (
+  `order_id` int(11) NOT NULL,
+  `order_number` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `order_table` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `order_gross_amount` decimal(12,2) NOT NULL,
+  `order_tax_amount` decimal(12,2) NOT NULL,
+  `order_net_amount` decimal(12,2) NOT NULL,
+  `order_date` date NOT NULL,
+  `order_time` time NOT NULL,
+  `order_waiter` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `order_cashier` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `order_status` enum('In Process','Completed') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_tax_table`
+--
+
+CREATE TABLE `order_tax_table` (
+  `order_tax_table_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `order_tax_name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `order_tax_percentage` decimal(4,2) NOT NULL,
+  `order_tax_amount` decimal(12,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_category_table`
+--
+
+CREATE TABLE `product_category_table` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `category_status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_table`
+--
+
+CREATE TABLE `product_table` (
+  `product_id` int(11) NOT NULL,
+  `category_name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `product_name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `product_price` decimal(10,2) NOT NULL,
+  `product_status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurant_table`
+--
+
+CREATE TABLE `restaurant_table` (
+  `restaurant_id` int(11) NOT NULL,
+  `restaurant_name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `restaurant_tag_line` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
+  `restaurant_address` text COLLATE utf8_unicode_ci NOT NULL,
+  `restaurant_contact_no` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `restaurant_email` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `restaurant_currency` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `restaurant_timezone` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `restaurant_logo` varchar(250) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_data`
+--
+
+CREATE TABLE `table_data` (
+  `table_id` int(11) NOT NULL,
+  `table_name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `table_capacity` int(3) NOT NULL,
+  `table_status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tax_table`
+--
+
+CREATE TABLE `tax_table` (
+  `tax_id` int(11) NOT NULL,
+  `tax_name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `tax_percentage` decimal(4,2) NOT NULL,
+  `tax_status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `user_contact_no` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `user_email` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `user_password` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `user_profile` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `user_type` enum('Master','Waiter','Cashier') COLLATE utf8_unicode_ci NOT NULL,
+  `user_status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL,
+  `user_created_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `order_item_table`
+--
+ALTER TABLE `order_item_table`
+  ADD PRIMARY KEY (`order_item_id`);
+
+--
+-- Indexes for table `order_table`
+--
+ALTER TABLE `order_table`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `order_tax_table`
+--
+ALTER TABLE `order_tax_table`
+  ADD PRIMARY KEY (`order_tax_table_id`);
+
+--
+-- Indexes for table `product_category_table`
+--
+ALTER TABLE `product_category_table`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `product_table`
+--
+ALTER TABLE `product_table`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `restaurant_table`
+--
+ALTER TABLE `restaurant_table`
+  ADD PRIMARY KEY (`restaurant_id`);
+
+--
+-- Indexes for table `table_data`
+--
+ALTER TABLE `table_data`
+  ADD PRIMARY KEY (`table_id`);
+
+--
+-- Indexes for table `tax_table`
+--
+ALTER TABLE `tax_table`
+  ADD PRIMARY KEY (`tax_id`);
+
+--
+-- Indexes for table `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `order_item_table`
+--
+ALTER TABLE `order_item_table`
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `order_table`
+--
+ALTER TABLE `order_table`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `order_tax_table`
+--
+ALTER TABLE `order_tax_table`
+  MODIFY `order_tax_table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `product_category_table`
+--
+ALTER TABLE `product_category_table`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `product_table`
+--
+ALTER TABLE `product_table`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `restaurant_table`
+--
+ALTER TABLE `restaurant_table`
+  MODIFY `restaurant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `table_data`
+--
+ALTER TABLE `table_data`
+  MODIFY `table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `tax_table`
+--
+ALTER TABLE `tax_table`
+  MODIFY `tax_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
